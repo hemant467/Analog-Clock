1c9bf206b53b6cf3abec9f3ba9c5f1cd43d562fe
⚔️Hemant@007⚔️ <85243370+hemant467@users.noreply.github.com>
2024-02-23 00:30:30 +0530

    Add files via upload

---
+++ Code ⌨️/Analog Clock ⌚.js
@@ -0,0 +1,24 @@
+// Get references to DOM elements
+const body = document.querySelector("body"),
+  hourHand = document.querySelector(".hour"),
+  minuteHand = document.querySelector(".minute"),
+  secondHand = document.querySelector(".second");
+
+const updateTime = () => {
+  // Get current time and calculate degrees for clock hands
+  let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360,
+    minToDeg = (date.getMinutes() / 60) * 360,
+    hrToDeg = (date.getHours() / 12) * 360;
+
+  // Rotate the clock hands to the appropriate degree based on the current time
+  secondHand.style.transform = `rotate(${secToDeg}deg)`;
+  minuteHand.style.transform = `rotate(${minToDeg}deg)`;
+  hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+};
+
+// call updateTime to set clock hands every second
+setInterval(updateTime, 1000);
+
+//call updateTime function on page load
+updateTime();
